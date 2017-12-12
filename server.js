4358a1c9c1b4a2b776c2eaccec788fdf5aa774e6
Ricky Amparo <rickyamparo@gmail.com>
2017-12-12 12:05:03 -0700

    create a server.js file and add the basic configuration, also npm i doby-parser

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Secret Box'
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+if(!module.parent) {
+  app.listen(app.get('port'), function() {
+    console.log(app.locals.title + " is running on " + app.get('port') + ".")
+  })
+}
+
+module.exports = app
