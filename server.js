9af2d580daec14a1d21ffea695339aca2200f09e
jayphodges <jayphodges@gmail.com>
2017-12-14 15:07:49 -0700

    Adjusting server settings for heroku

--- server.js
+++ server.js
@@ -35,7 +35,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next()
+  if (req.method === 'OPTIONS') {
+    res.send(200)
+  } else {
+    next()
+  }
 })
 
 // Foods
