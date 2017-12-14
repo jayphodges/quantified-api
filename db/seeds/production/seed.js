995a9780caa05d5206eafce2caad76a85aea5e7a
jayphodges <jayphodges@gmail.com>
2017-12-14 15:37:28 -0700

    Modifying seed file for deployment

--- db/seeds/production/seed.js
+++ db/seeds/production/seed.js
@@ -1,75 +1,63 @@
 exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE foods RESTART IDENTITY CASCADE;')
-    .then(function () {
-      return knex('foods').insert([
-        {name: 'Banana', calories: 150},
-        {name: 'Bagel Bites - Four Cheese', calories: 650},
-        {name: 'Chicken Burrito', calories: 800},
-        {name: 'Grapes', calories: 180},
-        {name: 'Blueberry Muffins', calories: 450},
-        {name: 'Yogurt', calories: 550},
-        {name: 'Macaroni and Cheese', calories: 950},
-        {name: 'Granola Bar', calories: 200},
-        {name: 'Gum', calories: 50},
-        {name: 'Cheese', calories: 400},
-        {name: 'Fruit Snacks', calories: 120},
-        {name: 'Apple', calories: 220}
-      ])
-    })
-}
-
-exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE foods RESTART IDENTITY CASCADE;')
-  .then(function() {
-    return Promise.all([
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Banana', 150]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Bagel Bites - Four Cheese', 650]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Chicken Burrito', 800]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Grapes', 180]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Blueberry Muffins', 450]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Yogurt', 550]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Macaroni and Cheese', 950]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Granola Bar', 200]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Gum', 50]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Cheese', 400]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Fruit Snacks', 120]
-        ),
-        knex.raw('INSERT INTO foods (name, calories) VALUES (?, ?)',
-          ['Apple', 220]
-        )
-    ])
-  })
-}
-
-exports.seed = function(knex, Promise) {
-  return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE;')
-    .then(function () {
-      return knex('meals').insert([
-        {name: 'Breakfast'},
-        {name: 'Lunch'},
-        {name: 'Dinner'},
-        {name: 'Snack'}
+  return knex.raw('DELETE FROM meal_foods; DELETE FROM meals; DELETE FROM foods')
+    .then(() => {
+      return Promise.all([
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Banana", 150, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Bagel Bites - Four Cheese", 650, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Chicken Burrito", 800, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Grapes", 180, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Blueberry Muffins", 450, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Yogurt", 550, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Macoroni and Cheese", 950, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Granola Bar", 200, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Gum", 50, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Cheese", 400, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Fruit Snack", 120, new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, ["Apple", 220, new Date, new Date]),
+        knex.raw(`INSERT INTO meals(name, created_at, updated_at)
+                  VALUES (?, ?, ?)`, ["Breakfast", new Date, new Date]),
+        knex.raw(`INSERT INTO meals(name, created_at, updated_at)
+                  VALUES (?, ?, ?)`, ["Lunch", new Date, new Date]),
+        knex.raw(`INSERT INTO meals(name, created_at, updated_at)
+                  VALUES (?, ?, ?)`, ["Dinner", new Date, new Date]),
+        knex.raw(`INSERT INTO meals(name, created_at, updated_at)
+                  VALUES (?, ?, ?)`, ["Snack", new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [8, 1, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [9, 1, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [10, 1, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [6, 2, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [11, 2, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [12, 2, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [1, 3, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [1, 3, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [9, 3, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [1, 4, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [1, 4, new Date, new Date]),
+        knex.raw(`INSERT INTO meal_foods(food_id, meal_id, created_at, updated_at)
+                  VALUES (?, ?, ?, ?)`, [5, 4, new Date, new Date])
       ])
     })
 }
