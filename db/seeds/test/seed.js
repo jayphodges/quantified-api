89f9fe18ef90c7dcab463e7b5d4f232c4a1ce1cf
Ricky Amparo <rickyamparo@gmail.com>
2017-12-13 19:51:42 -0700

    change seed file to reflect jp's changes

--- db/seeds/test/seed.js
+++ db/seeds/test/seed.js
@@ -1,5 +1,4 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex.raw('TRUNCATE foods RESTART IDENTITY CASCADE')
     .then(function () {
       return knex('foods').insert([
@@ -15,119 +14,18 @@ exports.seed = function(knex, Promise) {
         {name: 'Cheese', calories: 400},
         {name: 'Fruit Snacks', calories: 120},
         {name: 'Apple', calories: 220}
-      ]);
-    }).then(function() {
-      return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
-        .then(function () {
-          return knex('meals').insert([
-            {name: 'Breakfast'},
-            {name: 'Lunch'},
-            {name: 'Dinner'},
-            {name: 'Snack'}
-          ]);
-        })
-    }).then(function() {
-      return knex.raw('TRUNCATE foods_meals RESTART IDENTITY CASCADE')
-        .then(function () {
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [1, 1]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [2, 1]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [3, 1]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [4, 2]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [5, 2]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [6, 2]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [7, 3]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [8, 3]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [9, 3]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [10, 4]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [11, 4]
-          ),
-          knex.raw('INSERT INTO foods_meals (food_id, meal_id) VALUES (?, ?)',
-            [12, 4]
-          )
-        })
-      })
+      ])
+    })
 }
-//
-// exports.seed = function(knex, Promise) {
-//   let foods = [ ["Banana", 150], ["Bagel Bites - Four Cheese", 650]]
-//   return knex('foods').del()
-//   .then(function() {
-//     return Promise.all([
-//       foods.forEach(function (value) {
-//
-//         addFood(value)
-//             // let name = value[0]
-//             // let calories = value[1]
-//             // console.log(`${name}: ${calories}`)
-//             // knex.raw(
-//             // 'INSERT INTO foods (name, calories) VALUES (?, ?)',
-//             //     [name, calories])
-//         })
-//     ]);
-//   });
-// };
 
-// let foods = [ ["Banana", 150],
-//   ["Bagel Bites - Four Cheese", 650],
-//   ["Chicken Burrito", 800],
-//   ["Grapes", 180],
-//   ["Blueberry Muffins", 450],
-//   ["Yogurt", 550],
-//   ["Macaroni and Cheese", 950],
-//   ["Granola Bar", 200],
-//   ["Gum", 50],
-//   ["Cheese", 400],
-//   ["Fruit Snacks", 120],
-//   ["Apple", 220]]
-// const meals = [ "Breakfast", "Snack", "Lunch", "Dinner" ]
-//
-//
-// const addFood = (foods) => {
-//   console.log(this)
-//     foods.forEach(function (value) {
-//         let name = value[0]
-//         let calories = value[1]
-//         knex.raw(
-//         'INSERT INTO foods (name, calories) VALUES (?, ?)',
-//             [name, calories])
-//     })
-// }
-//
-// addFood(foods);
-// FOODS.each do |food|
-//   new_food = Food.create!(name: food.first, calories: food.last)
-//   puts "Created #{new_food.name}"
-// end
-//
-// MEALS.each do |meal_name|
-//   meal = Meal.create!(name: meal_name)
-//   puts "Created #{meal.name}"
-// end
-// const addFood = (foods)
-// Meal.all.each do |meal|
-//   3.times do
-//     random_id = rand(Food.first.id..Food.last.id)
-//     meal.foods << Food.find(random_id)
-//   end
-// end
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
+    .then(function () {
+      return knex('meals').insert([
+        {name: 'Breakfast'},
+        {name: 'Lunch'},
+        {name: 'Dinner'},
+        {name: 'Snack'}
+      ])
+    })
+}
