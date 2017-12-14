61d73c3a617e8817589dd32f15f4746a0d3fa65f
jayphodges <jayphodges@gmail.com>
2017-12-14 15:41:57 -0700

    Removed items in joins table from seed file

--- db/seeds/production/seed.js
+++ db/seeds/production/seed.js
@@ -2,38 +2,22 @@ exports.seed = function(knex, Promise) {
   return knex.raw('DELETE FROM meal_foods; DELETE FROM meals; DELETE FROM foods')
     .then(() => {
       return Promise.all([
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Banana", 150, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Bagel Bites - Four Cheese", 650, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Chicken Burrito", 800, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Grapes", 180, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Blueberry Muffins", 450, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Yogurt", 550, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Macoroni and Cheese", 950, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Granola Bar", 200, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Gum", 50, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Cheese", 400, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Fruit Snack", 120, new Date, new Date]),
-        knex.raw(`INSERT INTO foods(name, calories)
-                  VALUES (?, ?, ?, ?)`, ["Apple", 220, new Date, new Date]),
-        knex.raw(`INSERT INTO meals(name)
-                  VALUES (?, ?, ?)`, ["Breakfast", new Date, new Date]),
-        knex.raw(`INSERT INTO meals(name)
-                  VALUES (?, ?, ?)`, ["Lunch", new Date, new Date]),
-        knex.raw(`INSERT INTO meals(name)
-                  VALUES (?, ?, ?)`, ["Dinner", new Date, new Date]),
-        knex.raw(`INSERT INTO meals(name)
-                  VALUES (?, ?, ?)`, ["Snack", new Date, new Date]),
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Banana", 150]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Bagel Bites - Four Cheese", 650]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Chicken Burrito", 800]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Grapes", 180]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Blueberry Muffins", 450]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Yogurt", 550]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Macoroni and Cheese", 950]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Granola Bar", 200]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Gum", 50]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Cheese", 400]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Fruit Snack", 120]
+        knex.raw(`INSERT INTO foods(name, calories) VALUES (?, ?)`, ["Apple", 220]
+        knex.raw(`INSERT INTO meals(name) VALUES (?)`, ["Breakfast"]
+        knex.raw(`INSERT INTO meals(name) VALUES (?)`, ["Lunch"]
+        knex.raw(`INSERT INTO meals(name) VALUES (?)`, ["Dinner"]
+        knex.raw(`INSERT INTO meals(name) VALUES (?)`, ["Snack"]
       ])
     })
 }
